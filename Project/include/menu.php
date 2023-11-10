636634aef4c7716ad3900445ab7df218e013386e
CarlosH534 <127702984+CarlosH534@users.noreply.github.com>
2023-11-10 16:49:31 -0600

    Add files via upload

---
+++ Project/include/menu.php
@@ -0,0 +1,15 @@
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+  <div class="offcanvas-header bg-danger">
+    <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Menu</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body bg-success">
+    <ul class="list-group">
+      <li class="list-group-item bg-success">Perfil</li>
+      <li class="list-group-item bg-success">Reportes</li>
+      <li class="list-group-item bg-success">libros</li>
+      <li class="list-group-item bg-success">escribir</li>
+      <li class="list-group-item bg-success">editoriales</li>
+    </ul>
+  </div>
+</div>
